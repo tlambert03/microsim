7ed45b4843fb8e500e1f554e1cacc2522bd60dd8
Ashesh <ashesh276@gmail.com>
2024-05-19 22:33:01 +0200

    replaced Interval class with a function

--- src/microsim/interval_creation.py
+++ src/microsim/interval_creation.py
@@ -5,9 +5,6 @@ from typing import NamedTuple
 
 import numpy as np
 
-from microsim.schema._base_model import SimBaseModel
-
-
 class Bin(NamedTuple):
     """One interval."""
 
@@ -16,39 +13,28 @@ class Bin(NamedTuple):
     mean: float | None = None
     mode: float | None = None
 
+def generate_bins(x: np.ndarray, y: np.ndarray, numbins:int) -> list[Bin]:
+    """Divide the spectrum into intervals."""
+    return _generate_bins_equal_area(x, y, numbins)
 
-class BaseInterval(SimBaseModel):
-    num_clusters: int
-    bins: list[Bin] | None = None
+def _generate_bins_equal_area(x: np.ndarray, y: np.ndarray, numbins:int) -> list[Bin]:
+    bins = []
 
-    def generate_bins(self, x: np.ndarray, y: np.ndarray) -> None:
-        raise NotImplementedError
+    cumsum = np.cumsum(y)
+    step = cumsum[-1] / numbins
+    start_val = 0
+    end_vals = np.arange(step, cumsum[-1], step)
+    # Add the last bin if the last value is quite far from the last bin
+    end_vals = np.append(end_vals, cumsum[-1])
 
-    def get_bin_probablities(self, value) -> tuple[np.ndarray, np.ndarray]:
-        raise NotImplementedError
-
-
-class AreaBasedInterval(BaseInterval):
-    """Every interval has equal area under the curve."""
-
-    num_clusters: int
-
-    def generate_bins(self, x: np.ndarray, y: np.ndarray) -> list[Bin]:
-        self.bins = []
-
-        cumsum = np.cumsum(y)
-        step = cumsum[-1] / self.num_clusters
-        start_val = 0
-        end_vals = np.arange(step, cumsum[-1], step)
-        # Add the last bin if the last value is quite far from the last bin
-        end_vals = np.append(end_vals, cumsum[-1])
-
-        for idx, end_val in enumerate(end_vals):
-            mid_val = (start_val + end_val) / 2
-            # NOTE: minus 1 because we want the disjoint intervals. Also, for the last interval, we want the last index and so there is no minus 1.
-            end_idx = bisect_left(cumsum, end_val) - 1 * (idx != len(end_vals) - 1)
-            start_idx = bisect_left(cumsum, start_val)
-            # TODO: mid is not the mean.
-            mid_idx = bisect_left(cumsum, mid_val)
-            self.bins.append(Bin(start=x[start_idx], end=x[end_idx], mean=x[mid_idx]))
-            start_val = end_val
+    for idx, end_val in enumerate(end_vals):
+        mid_val = (start_val + end_val) / 2
+        # NOTE: minus 1 because we want the disjoint intervals. Also, for the last interval, we want the last index and so there is no minus 1.
+        end_idx = bisect_left(cumsum, end_val) - 1 * (idx != len(end_vals) - 1)
+        start_idx = bisect_left(cumsum, start_val)
+        # TODO: mid is not the mean.
+        mid_idx = bisect_left(cumsum, mid_val)
+        bins.append(Bin(start=x[start_idx], end=x[end_idx], mean=x[mid_idx]))
+        start_val = end_val
+    
+    return bins
