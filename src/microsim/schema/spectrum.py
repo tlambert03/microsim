4bfdb62eb0813c811ed85c1f28f16368f37ba422
Ashesh <ashesh276@gmail.com>
2024-05-20 00:27:53 +0200

    serializable numpy

--- src/microsim/schema/spectrum.py
+++ src/microsim/schema/spectrum.py
@@ -4,6 +4,7 @@ import numpy as np
 from pydantic import ConfigDict
 
 from ._base_model import SimBaseModel
+from .serializable_numpy import NdArray
 
 
 class Spectrum(SimBaseModel):
@@ -11,8 +12,8 @@ class Spectrum(SimBaseModel):
         "validate_assignment": True,
         "arbitrary_types_allowed": True,
     }
-    wavelength: np.ndarray  # nm
-    intensity: np.ndarray  # normalized to 1
+    wavelength: NdArray  # nm
+    intensity: NdArray  # normalized to 1
     scalar: float = 1  # scalar to multiply intensity by, such as EC or QY
 
     @property
