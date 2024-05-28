55ce979b61372cbc3ea97ce105dda62ab8e7a005
Talley Lambert <talley.lambert@gmail.com>
2024-05-28 14:19:31 -0400

    rm x

--- x.py
+++
@@ -1,8 +0,0 @@
-from microsim.schema import OpticalConfig
-from microsim.schema.optical_config.filter import Bandpass
-
-oc = OpticalConfig.from_fpbase("i6WL2WdgcDMgJYtPrpZcaJ::Widefield Dual Green")
-
-oc.plot()
-oc.excitation.plot()
-oc.emission.plot()
