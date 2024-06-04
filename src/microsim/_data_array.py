ad346d8ef2c8fc6508500d5a10bb38587bceaf65
Talley Lambert <talley.lambert@gmail.com>
2024-06-04 12:46:26 -0400

    refactor: update DataArray import in _data_array.py

--- src/microsim/_data_array.py
+++ src/microsim/_data_array.py
@@ -5,12 +5,23 @@ from typing import TYPE_CHECKING, Any, Protocol, runtime_checkable
 import xarray
 
 if TYPE_CHECKING:
+    from collections.abc import Hashable, Iterable, Mapping, Sequence
+
     import numpy as np
 
-try:
-    from .xarray_jax import DataArray
-except ImportError:
-    from xarray import DataArray  # type: ignore[assignment]
+    def DataArray(
+        data: Any,
+        coords: Sequence[Sequence | xarray.DataArray] | Mapping | None = None,
+        dims: str | Iterable[Hashable] | None = None,
+        name: Hashable | None = None,
+        attrs: Mapping | None = None,
+    ) -> xarray.DataArray: ...
+
+else:
+    try:
+        from .xarray_jax import DataArray
+    except ImportError:
+        from xarray import DataArray  # type: ignore[assignment]
 
 __all__ = ["DataArray", "xrDataArray", "ArrayProtocol"]
 
@@ -26,20 +37,19 @@ class DType(Protocol):
 
 @runtime_checkable
 class ArrayProtocol(Protocol):
+    """The basic protocol we need from an array-like object."""
+
     @property
     def dtype(self) -> np.dtype: ...
     @property
     def shape(self) -> tuple[int, ...]: ...
     @property
     def ndim(self) -> int: ...
+    @property
+    def size(self) -> int: ...
 
-    # strides: tuple[int, ...]
-    # size: int
-    # itemsize: int
-    # nbytes: int
-
-    # def __len__(self) -> int: ...
-    # def __getitem__(self, key: Any) -> Any: ...
-    # def __setitem__(self, key: Any, value: Any) -> None: ...
+    def __len__(self) -> int: ...
+    def __getitem__(self, key: Any) -> Any: ...
+    def __setitem__(self, key: Any, value: Any) -> None: ...
     def __array__(self) -> np.ndarray: ...
     def __mul__(self, other: Any) -> ArrayProtocol: ...
