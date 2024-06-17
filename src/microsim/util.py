a955b3dfdc8f896c79ebd0801689594eba266774
Talley Lambert <talley.lambert@gmail.com>
2024-06-17 12:56:54 -0400

    pin griffe

--- src/microsim/util.py
+++ src/microsim/util.py
@@ -310,7 +310,7 @@ def ortho_plot(
     try:
         fig.set_layout_engine("tight")
     except AttributeError:
-        fig.set_tight_layout(True)  # type: ignore
+        fig.set_tight_layout(True)
 
     if title:
         fig.suptitle(title)
